f17a0c7ea3d00098d1e10928c02a9cadff001117
RubemLuz <rubem.luz.santos@gmail.com>
2025-03-21 08:31:36 -0300

    Mudanças e Adiçoes
    Melhorando a estrutura e adicionando novos elementos e preparar novos conteúdos.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+const listImp = document.getElementById('listimp')
 
 let impressoras = []
-const listaImpressora = document.getElementById('listimp')
 
-function AddImp(modelo,local,ip,paginas)
+//Clear()
+LoadData()
+
+//Cria o objeto com base nos input (informações)
+function CreateObj(modelo,local,ip,paginas)
 {
     if(modelo != '' && local != '' && ip != '' && paginas != '')
     {
+        //ISSO AQUI É UM OBJETO!!!
         let impressora = 
         {
             Modelo: modelo,
@@ -19,7 +24,7 @@ function AddImp(modelo,local,ip,paginas)
         document.getElementById('ip').value = ''
         document.getElementById('page').value = ''
 
-        return salveImp(impressora)
+        return pushList(impressora)
     }
     else
     {
@@ -28,16 +33,53 @@ function AddImp(modelo,local,ip,paginas)
 
 }
 
-function salveImp(impObj)
+//Adiciona o objeto em uma Array
+function pushList(impObj)
 {
     impressoras.push(impObj)
+
+    SaveData(impObj)
 }
 
-function valuePag()         
+//Mostra uma lista com os objetos salvos na Array
+function showList()         
 {
-    let getImp = document.createElement("option")
-    getImp.text = `Impresora da Marca: ${impressoras[0].Modelo}, Se encontra em: ${impressoras[0].Local}, Com o IP: ${impressoras[0].Ip}, Com um total de: ${impressoras[0].Paginas} páginas`
-    return listaImpressora.appendChild(getImp)
-    
-    //return window.alert(impressoras[0].Modelo + impressoras[0].Local + impressoras[0].Ip + impressoras[0].Paginas)
+    listImp.innerHTML = ''
+
+    impressoras.sort((a,b) => a.Paginas.localeCompare(b.Paginas))
+
+    for (let i = 0; i < impressoras.length; i++)
+    {
+        let createList = document.createElement("option")
+        createList.text = `
+        Imp da Marca: ${impressoras[i].Modelo} --
+        Localização: ${impressoras[i].Local} -- 
+        IP: ${impressoras[i].Ip} -- 
+        Total de: ${impressoras[i].Paginas} páginas
+        `
+        listImp.appendChild(createList)
+    }    
+}
+
+function SaveData()
+{
+    localStorage.setItem('impObj', JSON.stringify(impressoras))
+}
+
+function LoadData()
+{
+    if(localStorage.getItem('impObj') == null){return}
+    else
+    {
+        const impObj = localStorage.getItem('impObj');
+
+        impressoras = JSON.parse(impObj);
+    }
+
+}
+
+function Clear()
+{
+    localStorage.clear('impObj')
+    localStorage.removeItem('impObj')
 }
