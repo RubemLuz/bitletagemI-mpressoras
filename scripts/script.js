a7d6d285497131a59989383009244657b40aca57
RubemLuz <rubem.luz.santos@gmail.com>
2025-05-03 00:00:15 -0300

    Melhorias e adições
    Usando llinks nos Ips para facillitar o acesso as impressoras na rede além de adicionar novas opções

--- scripts/script.js
+++ scripts/script.js
@@ -90,7 +90,7 @@ function allList()
     {
         let p = document.createElement("p")
         
-        p.textContent = `Marca da Impressora: ${impressoras.Modelo}, Se localiza em: ${impressoras.Local}, IP: ${impressoras.Ip}, Quantidade de Paginas: ${impressoras.Paginas}`
+        p.innerHTML = `Marca da Impressora: ${impressoras.Modelo}, Se localiza em: ${impressoras.Local}, IP: <a href="http://${impressoras.Ip}" target="_blank">${impressoras.Ip}</a>, Quantidade de Paginas: ${impressoras.Paginas}`
 
         listDiv.appendChild(p)
     });
@@ -100,7 +100,66 @@ function allList()
     listDiv.appendChild(final);
 }
 
+//Mostra uma lista com os objetos salvos na Array com ID
+function allListID()
+{
+    let listDiv = document.getElementById("all_listID");
+    let numID =0;
+    listDiv.innerHTML = "";
 
+    let initial = document.createElement("p");
+    initial.textContent = " ";
+    listDiv.appendChild(initial);
+    
+    switch (classificacao.value) 
+    {
+        case "0":
+            impressoras.sort((a,b) => a.Modelo.localeCompare(b.Modelo))
+            break;
+        case "1":
+            impressoras.sort((a,b) => a.Local.localeCompare(b.Local))
+            break;
+        case "2":
+            impressoras.sort((a,b) => a.Ip.localeCompare(b.Ip))
+            break;
+        case "3":
+            impressoras.sort((a,b) => a.Paginas.localeCompare(b.Paginas))
+            break;
+    }
+    impressoras.forEach(impressoras => 
+    {
+        let p = document.createElement("p")
+        numID++;
+        p.textContent = `ID: ${numID} ,Marca da Impressora: ${impressoras.Modelo}, Se localiza em: ${impressoras.Local}, IP: ${impressoras.Ip}, Quantidade de Paginas: ${impressoras.Paginas}`
+
+        listDiv.appendChild(p)
+    });
+    console.log('MANO????');
+    let final = document.createElement("p");
+    final.textContent = " ";
+    listDiv.appendChild(final);
+    return numID = 0;
+}
+
+function removeImp(id)
+{
+    if(id != 0 || id == null)
+    {
+        const confirmar = confirm(`Você selecionou o ID: ${id}! Deseja realmente deletar?`);
+        if(confirmar == true)
+        {
+            impressoras.splice(id-1,1);
+            //SaveData();
+            alert(`Impressora ID: ${id} deletada com sucesso!`);
+            allListID();
+        }
+        //return alert(`Você selecionou o ID: ${id}, da marca: ${impressoras[id-1].Modelo}, Se localiza em: ${impressoras[id-1].Local}, do IP: ${impressoras[id-1].Ip}, com ${impressoras[id-1].Paginas} paginas.`)
+    }
+    else
+    {
+        return alert("ID inválido");
+    }
+}
 //Manipulação de paginas
 function GoAddImp()
 {
